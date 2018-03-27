0ab54da106d502522eedf4cb089ce8167369181d
PengZhiHao <34024702+ChingCha@users.noreply.github.com>
2018-03-27 13:13:54 +0800

    20180327-01
    20180327-01

--- holtek_lcd/holtek_lcd/holtek_lcd.c
+++ holtek_lcd/holtek_lcd/holtek_lcd.c
@@ -1,125 +1,70 @@
-/*#include "HT66F50.h"
+#include	"HT66F50.h"
+#include <string.h>
 #define LCD_EN			_pe0
 #define LCD_ENC			_pec0
 #define LCD_RS			_pe1
 #define LCD_RSC			_pec1
-
+#define LCD_RW			_pe2
+#define LCD_RWC			_pec2
 #define LCD_DataPort	_pd
 #define LCD_DataPortC	_pdc
-void LCD_CMD(unsigned short lcmd);
 void LCD_Init(void);
-void LCD_WRITE(unsigned short data);
-void main()
-{
-	LCD_Init();
-	LCD_CMD(0x80);
-	LCD_WRITE(0x31);
-	while(1)
-	{
-		LCD_CMD(0x80);
-		LCD_WRITE(0x31);
-		GCC_DELAY(100000);
-		LCD_CMD(0xC0);
-		LCD_WRITE(0x32);
-		GCC_DELAY(100000);
-	}
-}
-
-void LCD_CMD(unsigned short cmd)
-{
-	LCD_RS = 0;
-	LCD_DataPort = cmd;
-	LCD_EN = 1;
-	GCC_DELAY(40);
-	LCD_EN = 0;
-	GCC_DELAY(40);
-	cmd <<= 4;
-	LCD_DataPort = cmd;
-	GCC_DELAY(40);
-	LCD_EN = 0;
-	GCC_DELAY(40);
-}
-void LCD_Init(void)
-{
-	LCD_RS = 0;
-	LCD_EN = 0;
-	LCD_RSC = 0;
-	LCD_ENC = 0;
-	LCD_DataPortC = 0;
-	GCC_DELAY(50000);
-	LCD_CMD(0x28);
-	LCD_CMD(0x0F);
-	LCD_CMD(0x06);
-	LCD_CMD(0x01);
-	GCC_DELAY(100000);
-}
-void LCD_WRITE(unsigned short data)
-{
-	LCD_RS = 1;
-	LCD_DataPort = data;
-	LCD_EN = 1;
-	GCC_DELAY(40);
-	LCD_EN = 0;
-	GCC_DELAY(40);
-	data <<= 4;
-	LCD_DataPort = data;
-	GCC_DELAY(40);
-	LCD_EN = 0;
-	GCC_DELAY(40);
-}*/
-// PROGRAM	: 5-6.c										   2011.0522
-// FUNCTION	: 20*2 LCM Contrl Demo Program 		   By Steven
-#include	"HT66F50.h"
-#define LCM_EN			_pe0
-#define LCM_ENC			_pec0
-#define LCM_RS			_pe1
-#define LCM_RSC			_pec1
-#define LCM_RW			_pe2
-#define LCM_RWC			_pec2
-#define LCM_DataPort	_pd
-#define LCM_DataPortC	_pdc
-void LCMInit(void);
-unsigned short LCMWrite(unsigned char, unsigned char);
+unsigned short LCD_Write(unsigned char, unsigned char);
 void PAUSE(unsigned short);
+static void Str_To_Lcd(char *pData, unsigned int count);
+void Lcd_Write_String(unsigned int line, unsigned int col, char *pText);
 void main()
 {	unsigned short i;
-	LCMInit();				                     //LCM Initialization
+	LCD_Init();				                     //LCM Initialization
 	while(1)
 	{	
-		LCMWrite(0,0x01);
+		LCD_Write(0,0x01);
 		PAUSE(100);
-		LCMWrite(0,0x80);						      //Clear LCM Display						
+		LCD_Write(0,0x80);						      //Clear LCM Display						
 		for(i=0;i<16;i++)
-		{	LCMWrite(1,0x31); PAUSE(10);        //Dispay "1"
+		{	Lcd_Write_String(1,i,"S"); PAUSE(10);        //Dispay "1"
 		}
-		LCMWrite(0,0xC0);					         //Set Line 2, Position 0
+		LCD_Write(0,0xC0);					         //Set Line 2, Position 0
 		for(i=0;i<16;i++) 
-		{	LCMWrite(1,0x32); PAUSE(10);	      //Dispay "2"
+		{	LCD_Write(1,0x32); PAUSE(10);	      //Dispay "2"
 		}
 		PAUSE(100);  
 	}
 }
-void LCMInit(void)
-{	LCM_EN=0; LCM_RW=0; LCM_RS=0;	            //Clear EN/RW/RS Pin
-	LCM_ENC=0; LCM_RWC=0; LCM_RSC=0;	         //Config EN/RW/RS O/PAUSE
+void LCD_Init(void)
+{	LCD_EN=0; LCD_RW=0; LCD_RS=0;	            //Clear EN/RW/RS Pin
+	LCD_ENC=0; LCD_RWC=0; LCD_RSC=0;	         //Config EN/RW/RS O/PAUSE
 	GCC_DELAY(50000);							         //LCM Power-On Time
-	LCMWrite(0,0x28);							      //COMD:8-Bit, 2-Line, 5*7
-	LCMWrite(0,0x0F);						         //COMD:Display/Cursor Blinking On
-	LCMWrite(0,0x06);							      //COMD:Increment, Not Shift
-	LCMWrite(0,0x01);							      //COMD:Clear LCM Display
+	LCD_Write(0,0x28);							      //COMD:8-Bit, 2-Line, 5*7
+	LCD_Write(0,0x0F);						         //COMD:Display/Cursor Blinking On
+	LCD_Write(0,0x06);							      //COMD:Increment, Not Shift
+	LCD_Write(0,0x01);							      //COMD:Clear LCM Display
 }
-unsigned short LCMWrite(unsigned char DCFlag, unsigned char CD)
-{	unsigned short BusyAC;
-	LCM_DataPortC=0;								   //Config Port as O/P Mode
-	LCM_RW=0;										   //RW=0 (Write)
-	LCM_RS=DCFlag;									   //Set RS State By DCFlag
-	LCM_DataPort=CD;								   //Sent Command or Data
-	LCM_EN=1; _nop(); LCM_EN=0;			      //EN=1
-	LCM_DataPort= CD << 4;								   //Sent Command or Data
-	LCM_EN=1; _nop(); LCM_EN=0;			      //EN=1
-	return BusyAC;	
+unsigned short LCD_Write(unsigned char DCFlag, unsigned char CD)
+{	
+	LCD_DataPortC=0;								   //Config Port as O/P Mode
+	LCD_RW=0;										   //RW=0 (Write)
+	LCD_RS=DCFlag;									   //Set RS State By DCFlag
+	LCD_DataPort=CD;								   //Sent Command or Data
+	LCD_EN=1; _nop(); LCD_EN=0;			      //EN=1
+	LCD_DataPort= CD << 4;								   //Sent Command or Data
+	LCD_EN=1; _nop(); LCD_EN=0;			      //EN=1	
 }
 void PAUSE(unsigned short i)						
 {	unsigned short j;
 	for (j=0;j<i;j++) GCC_DELAY(10000);				//Delay i*10mS 
+}
+
+static void Str_To_Lcd(char *pData, unsigned int count)
+{
+    while (count--)
+    {
+        LCD_Write(1,*(pData++));
+    }
+}
+void Lcd_Write_String(unsigned int line, unsigned int col, char *pText)
+{
+	if(line == 1) 	LCD_Write(0,0x80 + col);
+	if(line == 2) 	LCD_Write(0,0xC0 + col);
+	Str_To_Lcd(pText, strlen(pText));
 }
